3d74704b07af77aa5c554c40176ced74c1077ff8
Waldemar Szymon <walldy@tlen.pl>
2023-04-03 12:21:34 +0200

    [Kodilla task 19.4] Modify Home page

--- src/components/features/Posts/Posts.js
+++ src/components/features/Posts/Posts.js
@@ -1,23 +1,30 @@
 import { getAllPosts } from "../../../redux/postsRedux";
 import { useSelector } from "react-redux";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Row, Col, Card, Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const Posts = () => {
 
   const posts = useSelector(getAllPosts);
 
   return (
-    <Container>
-      <Row>
-        {posts.map(post => (
-          <Col key={post.id} lg={4} md={6} sm={12} className='border rounded'>
-            <h5>{post.title}</h5>
-          </Col>
-      ))}
-      </Row>
-    </Container>
+    <Row>
+      {posts.map(post => (
+        <Col key={post.id} lg={4} md={6} sm={12}>
+          <Card key={post.id} className='mt-3'>
+            <Card.Body>
+              <Card.Title>{post.title}</Card.Title>
+              <Card.Text>
+                <strong>Author:&nbsp;</strong>{post.author}<br />
+                <strong>Published:&nbsp;</strong>{post.publishedDate}
+              </Card.Text>
+              <Card.Text>{post.shortDescription}</Card.Text>
+              <Button variant='primary' as={NavLink} to={'/post/' + post.id}>Read more</Button>
+            </Card.Body>
+          </Card>
+      </Col>
+    ))}
+    </Row>
   );
 };
 
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,11 +1,16 @@
 import Posts from "../../features/Posts/Posts";
+import { Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
 
   return (
     <div>
-      <h1>All posts</h1>
-      <Posts />
+      <div className="d-flex justify-content-between">
+        <h2>All posts</h2>
+        <Button variant='outline-primary' as={NavLink} to={'/post/add'}>Add post</Button>
+      </div>
+    <Posts />
     </div>
   );
 };
